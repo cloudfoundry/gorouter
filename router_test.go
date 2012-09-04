a93325759b396ef84e6090a54c04df059a89f95c
Anfernee Gui <agui@vmware.com>
2012-09-04 16:22:45 +0800

    Supressing log output for go test
    Change-Id: I8a8fea7c170b16174db9d5ad874d58878cdba996

--- router_test.go
+++ router_test.go
@@ -2,9 +2,14 @@ package router
 
 import (
 	. "launchpad.net/gocheck"
+	"log"
+	"os"
 	"testing"
 )
 
 func Test(t *testing.T) {
+	file, _ := os.OpenFile("/dev/null", os.O_WRONLY, 0666)
+	log.SetOutput(file)
+
 	TestingT(t)
 }
