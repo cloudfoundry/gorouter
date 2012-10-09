882e4db73d8b1d33b4ece58208b8fb1774e6de87
Jeff Li <jeffli@rbcon.com>
2012-10-09 15:36:15 +0800

    Remove some unnecessary performance tests
    These performace test cases are hardly equivalent to real-word usage
    
    Change-Id: I7f774b91bbb066aa148847858da4420a3545bd79

--- perf_test.go
+++ perf_test.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"encoding/base64"
-	"net/http"
 	"strconv"
 	"testing"
 )
@@ -48,66 +47,4 @@ func BenchmarkRegister(b *testing.B) {
 		}
 		p.Register(rm)
 	}
-}
-
-func BenchmarkProxy(b *testing.B) {
-	b.StopTimer()
-
-	// Start app
-	server := &http.Server{
-		Addr: ":40899",
-	}
-	go server.ListenAndServe()
-
-	// New Proxy
-	s, _ := NewAESSessionEncoder([]byte(SessionKey), base64.StdEncoding)
-	p := NewProxy(s)
-	p.status = NewServerStatus()
-
-	// Register app
-	rm := &registerMessage{
-		Host: "localhost",
-		Port: 40899,
-		Uris: []string{"bench.vcap.me"},
-		Tags: map[string]string{"component": "cc", "runtime": "ruby"},
-	}
-	p.Register(rm)
-
-	// Load 10000 registered apps
-	for i := 0; i < 10000; i++ {
-		str := strconv.Itoa(i)
-		rm := &registerMessage{
-			Host: "localhost",
-			Port: uint16(i),
-			Uris: []string{"bench.vcap.me." + str},
-		}
-		p.Register(rm)
-	}
-
-	// New request and response writer
-	req, _ := http.NewRequest("GET", "bench.vcap.me", nil)
-	req.Host = "bench.vcap.me"
-	rw := new(NullResponseWriter)
-
-	b.StartTimer()
-
-	for i := 0; i < b.N; i++ {
-		p.ServeHTTP(rw, req)
-	}
-}
-
-type NullResponseWriter struct {
-	header http.Header
-}
-
-func (rw *NullResponseWriter) Header() http.Header {
-	return rw.header
-}
-
-func (rw *NullResponseWriter) Write(b []byte) (int, error) {
-	return 0, nil
-}
-
-func (rw *NullResponseWriter) WriteHeader(i int) {
-	// do nothing
 }
