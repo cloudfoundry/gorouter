6a4b2758f477762b965d67fdc698dbab1698a0f9
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-11-21 18:21:56 -0800

    Remove custom JSON marshalling code
    The new approach is much less efficient, but allows the user to override
    the MarshalJSON function to implement custom marshalling.
    
    This could have been added to the custom code, but is a pain to
    maintain. Ideally, we change the varz format down the line to allow for
    easier composition.
    
    Change-Id: Ied8721a9ca62fe95c86660fa32747bdbbc019cda

--- src/router/common/varz.go
+++ src/router/common/varz.go
@@ -2,14 +2,10 @@ package common
 
 import (
 	"encoding/json"
-	"reflect"
-	"strings"
 	"sync"
 )
 
-type Varz struct {
-	sync.Mutex `json:"-"`
-
+type varz_ struct {
 	// Static common metrics
 	NumCores int `json:"num_cores"`
 
@@ -18,78 +14,53 @@ type Varz struct {
 	Cpu     float64 `json:"cpu"`
 
 	Uptime Duration `json:"uptime"`
+}
+
+type Varz struct {
+	sync.Mutex
+
+	varz_
 
 	// Every component's unique metrics
-	UniqueVarz interface{} `encode:"yes"`
+	UniqueVarz interface{}
+}
+
+func transform(x interface{}, y *map[string]interface{}) error {
+	var b []byte
+	var err error
+
+	b, err = json.Marshal(x)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(b, y)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (v *Varz) MarshalJSON() ([]byte, error) {
-	d := parseVarz(v)
+	r := make(map[string]interface{})
 
-	// Merge component's information from VcapComponent
-	c := parseVarz(Component)
-	for k, v := range c {
-		if d[k] == nil { // prevent fields of varz being overridden
-			d[k] = v
-		}
+	var err error
+
+	err = transform(v.UniqueVarz, &r)
+	if err != nil {
+		return nil, err
 	}
 
-	return json.Marshal(d)
-}
-
-func parseVarz(varz interface{}) map[string]interface{} {
-	data := make(map[string]interface{})
-
-	parseVarzRecursively(varz, data)
-
-	return data
-}
-
-func parseVarzRecursively(varz interface{}, data map[string]interface{}) {
-	typeInfo := reflect.TypeOf(varz)
-	var value reflect.Value
-	if typeInfo.Kind() == reflect.Ptr {
-		typeInfo = typeInfo.Elem()
-		value = reflect.ValueOf(varz).Elem()
-	} else {
-		value = reflect.ValueOf(varz)
+	err = transform(v.varz_, &r)
+	if err != nil {
+		return nil, err
 	}
 
-	if typeInfo.Kind() != reflect.Struct {
-		return
+	err = transform(Component, &r)
+	if err != nil {
+		return nil, err
 	}
 
-	for i := 0; i < typeInfo.NumField(); i++ {
-		t := typeInfo.Field(i)
-
-		if !isExported(t.Name) {
-			continue
-		}
-
-		k := t.Tag.Get("json")
-		if k == "-" {
-			continue
-		}
-
-		if t.Tag.Get("encode") == "yes" {
-			parseVarzRecursively(value.Field(i).Interface(), data)
-		} else {
-			if k == "" {
-				k = strings.ToLower(t.Name)
-			}
-			data[k] = value.Field(i).Interface()
-		}
-	}
-}
-
-func isExported(name string) bool {
-	if len(name) < 1 {
-		return false
-	}
-
-	if name[0] >= 'A' && name[0] <= 'Z' {
-		return true
-	}
-
-	return false
+	return json.Marshal(r)
 }
