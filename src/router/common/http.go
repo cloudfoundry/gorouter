cbc4f9102a8ccbed763548e7d664e16a0c6e2f9b
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-11-28 13:49:56 -0800

    Extract a basic authentication handler to router/common/http
    Change-Id: I1d72f626b27ed99aa9aabfd6804a5b734ef8dbf2

--- src/router/common/http.go
+++ src/router/common/http.go
@@ -1,47 +1,10 @@
 package common
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"net/http"
-	"strings"
+	. "router/common/http"
 )
-
-type BasicAuth struct {
-	handler http.Handler
-}
-
-func (a *BasicAuth) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	if !checkAuth(req, Component.Credentials[0], Component.Credentials[1]) {
-		w.Header().Set("WWW-Authenticate", "Basic")
-		w.WriteHeader(401)
-		w.Write([]byte("401 Unauthorized\n"))
-	} else {
-		a.handler.ServeHTTP(w, req)
-	}
-}
-
-func checkAuth(req *http.Request, user string, password string) bool {
-	if user == "" && password == "" {
-		return true
-	}
-
-	authParts := strings.Split(req.Header.Get("Authorization"), " ")
-	if len(authParts) != 2 || authParts[0] != "Basic" {
-		return false
-	}
-
-	code, err := base64.StdEncoding.DecodeString(authParts[1])
-	if err != nil {
-		return false
-	}
-
-	userPass := strings.Split(string(code), ":")
-	if len(userPass) != 2 || userPass[0] != user || userPass[1] != password {
-		return false
-	}
-	return true
-}
 
 func startStatusServer() {
 	mux := http.NewServeMux()
@@ -61,13 +24,13 @@ func startStatusServer() {
 		enc.Encode(UpdateVarz())
 	})
 
-	basicAuth := &BasicAuth{
-		handler: mux,
+	f := func(user, password string) bool {
+		return user == Component.Credentials[0] && password == Component.Credentials[1]
 	}
 
 	server := &http.Server{
 		Addr:    Component.Host,
-		Handler: basicAuth,
+		Handler: &BasicAuth{mux, f},
 	}
 
 	server.ListenAndServe()
