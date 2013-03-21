c16724d640af9c28d1f50fa00323999390e4bbd6
Bob Nugmanov and Dmitriy Kalinin <pair+bob+dmitriy@pivotallabs.com>
2013-03-21 11:24:36 -0700

    stop using global ServeMux

--- src/router/common/http.go
+++ src/router/common/http.go
@@ -7,8 +7,10 @@ import (
 	. "router/common/http"
 )
 
-func init() {
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+func startStatusServer() {
+	hs := http.NewServeMux()
+
+	hs.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 
@@ -16,23 +18,21 @@ func init() {
 		enc.Encode(UpdateHealthz())
 	})
 
-	http.HandleFunc("/varz", func(w http.ResponseWriter, req *http.Request) {
+	hs.HandleFunc("/varz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 
 		enc := json.NewEncoder(w)
 		enc.Encode(UpdateVarz())
 	})
-}
 
-func startStatusServer() {
 	f := func(user, password string) bool {
 		return user == Component.Credentials[0] && password == Component.Credentials[1]
 	}
 
 	s := &http.Server{
 		Addr:    Component.Host,
-		Handler: &BasicAuth{http.DefaultServeMux, f},
+		Handler: &BasicAuth{hs, f},
 	}
 
 	s.ListenAndServe()
