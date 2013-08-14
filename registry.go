b28504f7138579c182b19d62dd30cb244f48594f
Mark Rushakoff and Tim Labeeuw <pair+mrushakoff+tim@pivotallabs.com>
2013-08-14 16:05:29 -0700

    Move URIs to their own file

--- registry.go
+++ registry.go
@@ -8,60 +8,9 @@ import (
 	"github.com/cloudfoundry/gorouter/util"
 	steno "github.com/cloudfoundry/gosteno"
 	"math/rand"
-	"strings"
 	"sync"
 	"time"
 )
-
-type Uri string
-type Uris []Uri
-
-func (u Uri) ToLower() Uri {
-	return Uri(strings.ToLower(string(u)))
-}
-
-func (ms Uris) Sub(ns Uris) Uris {
-	var rs Uris
-
-	for _, m := range ms {
-		found := false
-		for _, n := range ns {
-			if m == n {
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			rs = append(rs, m)
-		}
-	}
-
-	return rs
-}
-
-func (x Uris) Has(y Uri) bool {
-	for _, xb := range x {
-		if xb == y {
-			return true
-		}
-	}
-
-	return false
-}
-
-func (x Uris) Remove(y Uri) (Uris, bool) {
-	for i, xb := range x {
-		if xb == y {
-			x[i] = x[len(x)-1]
-			x = x[:len(x)-1]
-			return x, true
-		}
-	}
-
-	return x, false
-}
-
 
 // This is a transient struct. It doesn't maintain state.
 type registryMessage struct {
---
+++ uris.go
@@ -0,0 +1,55 @@
+package router
+
+import (
+	"strings"
+)
+
+type Uri string
+
+func (u Uri) ToLower() Uri {
+	return Uri(strings.ToLower(string(u)))
+}
+
+type Uris []Uri
+
+func (ms Uris) Sub(ns Uris) Uris {
+	var rs Uris
+
+	for _, m := range ms {
+		found := false
+		for _, n := range ns {
+			if m == n {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			rs = append(rs, m)
+		}
+	}
+
+	return rs
+}
+
+func (x Uris) Has(y Uri) bool {
+	for _, xb := range x {
+		if xb == y {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (x Uris) Remove(y Uri) (Uris, bool) {
+	for i, xb := range x {
+		if xb == y {
+			x[i] = x[len(x)-1]
+			x = x[:len(x)-1]
+			return x, true
+		}
+	}
+
+	return x, false
+}
