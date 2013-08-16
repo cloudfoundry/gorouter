9424a0e655d03c1fcdff461a437a0a9189fc7aec
Alex Suraci and Mark Rushakoff <pair+alex+mrushakoff@pivotallabs.com>
2013-08-15 18:09:00 -0700

    fix some flakiness in integration spec

--- integration_test.go
+++ integration_test.go
@@ -43,8 +43,12 @@ func (s *IntegrationSuite) TestNatsConnectivity(c *C) {
 	s.mbusClient = s.router.mbusClient
 
 	heartbeatInterval := 200 * time.Millisecond
-	staleThreshold := 1 * time.Second
+
+	// ensure the threshold is longer than the interval that we check;
+	// because we set the route's timestamp to time.Now() on the interval
+	// as part of pausing
 	staleCheckInterval := s.router.registry.pruneStaleDropletsInterval
+	staleThreshold := 2 * staleCheckInterval
 
 	s.router.registry.dropletStaleThreshold = staleThreshold
 
@@ -54,8 +58,8 @@ func (s *IntegrationSuite) TestNatsConnectivity(c *C) {
 	runningApp := test.NewGreetApp([]string{"test.nats.dying.and.app.stays.alive.vcap.me"}, proxyPort, s.mbusClient, nil)
 	runningApp.Listen()
 
-	c.Assert(s.waitAppRegistered(zombieApp, time.Second*2), Equals, true)
-	c.Assert(s.waitAppRegistered(runningApp, time.Second*2), Equals, true)
+	c.Assert(s.waitAppRegistered(zombieApp, 2*time.Second), Equals, true)
+	c.Assert(s.waitAppRegistered(runningApp, 2*time.Second), Equals, true)
 
 	zombieTicker := time.NewTicker(heartbeatInterval)
 	runningTicker := time.NewTicker(heartbeatInterval)
@@ -90,34 +94,65 @@ func (s *IntegrationSuite) TestNatsConnectivity(c *C) {
 	zombieApp.VerifyAppStatus(200, c)
 	runningApp.VerifyAppStatus(200, c)
 
-	// Wait the regular stale timeout
-	time.Sleep(staleCheckInterval + staleThreshold + 250*time.Millisecond)
+	zombieGone := make(chan bool)
 
-	// Finally the zombie is cleaned up. Maybe proactively enqueue Unregister events in DEA's.
-	zombieApp.VerifyAppStatus(404, c)
-	runningApp.VerifyAppStatus(200, c)
+	go func() {
+		for {
+			// Finally the zombie is cleaned up. Maybe proactively enqueue Unregister events in DEA's.
+			err := zombieApp.CheckAppStatus(404)
+			if err != nil {
+				time.Sleep(100 * time.Millisecond)
+				continue
+			}
+
+			err = runningApp.CheckAppStatus(200)
+			if err != nil {
+				time.Sleep(100 * time.Millisecond)
+				continue
+			}
+
+			zombieGone <- true
+
+			break
+		}
+	}()
+
+	select {
+	case <-zombieGone:
+	case <-time.After(staleCheckInterval + staleThreshold + 5*time.Second):
+		c.Error("Zombie app was not pruned.")
+	}
 }
 
-func (s *IntegrationSuite) waitMsgReceived(a *test.TestApp, r bool, t time.Duration) bool {
-	i := time.Millisecond * 50
-	m := int(t / i)
+func (s *IntegrationSuite) waitMsgReceived(a *test.TestApp, shouldBeRegistered bool, t time.Duration) bool {
+	registeredOrUnregistered := make(chan bool)
 
-	for j := 0; j < m; j++ {
-		received := true
-		for _, v := range a.Urls() {
-			_, ok := s.router.registry.Lookup(v)
-			if ok != r {
-				received = false
+	go func() {
+		for {
+			received := true
+			for _, v := range a.Urls() {
+				_, ok := s.router.registry.Lookup(v)
+				if ok != shouldBeRegistered {
+					received = false
+					break
+				}
+			}
+
+			if received {
+				registeredOrUnregistered <- true
 				break
 			}
-		}
-		if received {
-			return true
-		}
-		time.Sleep(i)
-	}
 
-	return false
+			time.Sleep(50 * time.Millisecond)
+		}
+	}()
+
+	select {
+	case <-registeredOrUnregistered:
+		return true
+	case <-time.After(t):
+		return false
+	}
 }
 
 func (s *IntegrationSuite) waitAppRegistered(app *test.TestApp, timeout time.Duration) bool {
