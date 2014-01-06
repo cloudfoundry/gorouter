1ef32835b68cd01645f1e0bb92496c179373268f
Ruth Byers <rbyers@pivotallabs.com>
2014-01-06 14:06:09 -0800

    Copy specs for extracted behavior
    [#63074062]
    
    Signed-off-by: Abhijit Hiremagalur <abhi@pivotallabs.com>

---
+++ access_log/create_running_access_logger_test.go
@@ -0,0 +1,41 @@
+package access_log
+
+import (
+	. "launchpad.net/gocheck"
+	"github.com/cloudfoundry/gorouter/config"
+)
+
+type CreateRunningAccessLoggerSuite struct{}
+
+func (s *CreateRunningAccessLoggerSuite) TestProxyHasNoAccessLoggerIfNoAccesLogAndNoLoggregatorUrl(c *C) {
+	config := config.DefaultConfig()
+	c.Assert(CreateRunningAccessLogger(config), IsNil)
+}
+
+func (s *CreateRunningAccessLoggerSuite) TestProxyHasAccessLoggerIfNoAccesLogButLoggregatorUrl(c *C) {
+	config := config.DefaultConfig()
+	config.LoggregatorConfig.Url = "10.10.3.13:4325"
+	config.AccessLog = ""
+	c.Assert(CreateRunningAccessLogger(config), NotNil)
+}
+
+func (s *CreateRunningAccessLoggerSuite) TestProxyHasAccessLoggerIfAccesLogButNoLoggregatorUrl(c *C) {
+	config := config.DefaultConfig()
+	config.AccessLog = "/dev/null"
+	c.Assert(CreateRunningAccessLogger(config), NotNil)
+}
+
+func (s *CreateRunningAccessLoggerSuite) TestProxyHasAccessLoggerIfBothAccesLogAndLoggregatorUrl(c *C) {
+	config := config.DefaultConfig()
+	config.LoggregatorConfig.Url = "10.10.3.13:4325"
+	config.AccessLog = "/dev/null"
+	c.Assert(CreateRunningAccessLogger(config), NotNil)
+}
+
+func (s *CreateRunningAccessLoggerSuite) TestProxyPanicsIfInvalidAccessLogLocation(c *C) {
+	config := config.DefaultConfig()
+	config.AccessLog = "/this\\should/panic"
+	c.Assert(func() {
+			CreateRunningAccessLogger(config)
+		}, PanicMatches, "open /this\\\\should/panic: no such file or directory")
+}
