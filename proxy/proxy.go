a9044d1f87988250ec3a69b26a83fdc9a8d49735
Krum Tsvetkov <krum.tsvetkov@sap.com>
2015-12-02 15:46:22 +0100

    Fixes #106 - Allow HTTP request target in absolute-form
    If the router recieves a request like
      GET http://my.test.io/my-path HTTP/1.1
      Host: my.test.io
    
    an error message "Requested route ('my.test.io') does not exist." is returned, even though the route my.test.io exists.
    
    This patch provides a test and fix for the issue.

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -125,7 +125,14 @@ func (p *proxy) getStickySession(request *http.Request) string {
 }
 
 func (p *proxy) lookup(request *http.Request) *route.Pool {
-	uri := route.Uri(hostWithoutPort(request) + request.RequestURI)
+	var requestPath string
+	if request.URL.IsAbs() {
+		requestPath = request.URL.Path
+	} else {
+		requestPath = request.RequestURI
+	}
+
+	uri := route.Uri(hostWithoutPort(request) + requestPath)
 	return p.registry.Lookup(uri)
 }
 
