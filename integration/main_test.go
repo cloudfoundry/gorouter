1cf07422d55c685a71df5c30277f9e06eedab711
Zachary Gershman <zgershman@pivotal.io>
2018-10-24 14:24:15 -0700

    extract tls to backends integration tests

--- integration/main_test.go
+++ integration/main_test.go
@@ -2,11 +2,9 @@ package integration
 
 import (
 	"crypto/tls"
-	"crypto/x509"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -21,7 +19,6 @@ import (
 	"syscall"
 	"time"
 
-	"code.cloudfoundry.org/gorouter/common/uuid"
 	"code.cloudfoundry.org/gorouter/config"
 	"code.cloudfoundry.org/gorouter/handlers"
 	"code.cloudfoundry.org/gorouter/mbus"
@@ -210,164 +207,6 @@ var _ = Describe("Router Integration", func() {
 			Eventually(func() string {
 				return string(gorouterSession.Out.Contents())
 			}).Should(ContainSubstring(`"routing_table_sharding_mode":"all"`))
-		})
-	})
-
-	Describe("TLS to backends", func() {
-		var (
-			serverCertDomainSAN string
-			backendCertChain    test_util.CertChain // server cert presented by backend to gorouter
-			clientCertChain     test_util.CertChain // client cert presented by gorouter to backend
-			backendTLSConfig    *tls.Config
-			mbusClient          *nats.Conn
-		)
-
-		BeforeEach(func() {
-			cfg = createConfig(cfgFile, defaultPruneInterval, defaultPruneThreshold, 0, false, 10, natsPort)
-			cfg.CipherSuites = []uint16{tls.TLS_RSA_WITH_AES_256_CBC_SHA}
-			cfg.SkipSSLValidation = false
-
-			serverCertDomainSAN, _ = uuid.GenerateUUID()
-			backendCertChain = test_util.CreateSignedCertWithRootCA(test_util.CertNames{CommonName: serverCertDomainSAN})
-			cfg.CACerts = string(backendCertChain.CACertPEM)
-
-			clientCertChain = test_util.CreateSignedCertWithRootCA(test_util.CertNames{CommonName: "gorouter"})
-			backendTLSConfig = backendCertChain.AsTLSConfig()
-			backendTLSConfig.ClientAuth = tls.RequireAndVerifyClientCert
-
-			// set Gorouter to use client certs
-			cfg.Backends.TLSPem = config.TLSPem{
-				CertChain:  string(clientCertChain.CertPEM),
-				PrivateKey: string(clientCertChain.PrivKeyPEM),
-			}
-
-			// make backend trust the CA that signed the gorouter's client cert
-			certPool := x509.NewCertPool()
-			certPool.AddCert(clientCertChain.CACert)
-			backendTLSConfig.ClientCAs = certPool
-		})
-
-		JustBeforeEach(func() {
-			var err error
-			writeConfig(cfg, cfgFile)
-			mbusClient, err = newMessageBus(cfg)
-			Expect(err).ToNot(HaveOccurred())
-
-			gorouterSession = startGorouterSession(cfgFile)
-		})
-
-		AfterEach(func() {
-			gorouterSession.Kill()
-		})
-		It("successfully establishes a mutual TLS connection with backend", func() {
-			runningApp1 := test.NewGreetApp([]route.Uri{"some-app-expecting-client-certs." + test_util.LocalhostDNS}, proxyPort, mbusClient, nil)
-			runningApp1.TlsRegister(serverCertDomainSAN)
-			runningApp1.TlsListen(backendTLSConfig)
-			heartbeatInterval := 200 * time.Millisecond
-			runningTicker := time.NewTicker(heartbeatInterval)
-			done := make(chan bool, 1)
-			defer func() { done <- true }()
-			go func() {
-				for {
-					select {
-					case <-runningTicker.C:
-						runningApp1.TlsRegister(serverCertDomainSAN)
-					case <-done:
-						return
-					}
-				}
-			}()
-			routesUri := fmt.Sprintf("http://%s:%s@%s:%d/routes", cfg.Status.User, cfg.Status.Pass, localIP, statusPort)
-
-			Eventually(func() bool { return appRegistered(routesUri, runningApp1) }, "2s").Should(BeTrue())
-			runningApp1.VerifyAppStatus(200)
-		})
-
-		Context("websockets and TLS interaction", func() {
-			assertWebsocketSuccess := func(wsApp *common.TestApp) {
-				routesUri := fmt.Sprintf("http://%s:%s@%s:%d/routes", cfg.Status.User, cfg.Status.Pass, localIP, statusPort)
-
-				Eventually(func() bool { return appRegistered(routesUri, wsApp) }, "2s").Should(BeTrue())
-
-				conn, err := net.Dial("tcp", fmt.Sprintf("ws-app.%s:%d", test_util.LocalhostDNS, cfg.Port))
-				Expect(err).NotTo(HaveOccurred())
-
-				x := test_util.NewHttpConn(conn)
-
-				req := test_util.NewRequest("GET", "ws-app."+test_util.LocalhostDNS, "/chat", nil)
-				req.Header.Set("Upgrade", "websocket")
-				req.Header.Set("Connection", "upgrade")
-				x.WriteRequest(req)
-
-				resp, _ := x.ReadResponse()
-				Expect(resp.StatusCode).To(Equal(http.StatusSwitchingProtocols))
-
-				x.WriteLine("hello from client")
-				x.CheckLine("hello from server")
-
-				x.Close()
-			}
-
-			It("successfully connects with both websockets and TLS to backends", func() {
-				wsApp := test.NewWebSocketApp([]route.Uri{"ws-app." + test_util.LocalhostDNS}, proxyPort, mbusClient, time.Millisecond, "")
-				wsApp.TlsRegister(serverCertDomainSAN)
-				wsApp.TlsListen(backendTLSConfig)
-
-				assertWebsocketSuccess(wsApp)
-			})
-
-			It("successfully connects with websockets but not TLS to backends", func() {
-				wsApp := test.NewWebSocketApp([]route.Uri{"ws-app." + test_util.LocalhostDNS}, proxyPort, mbusClient, time.Millisecond, "")
-				wsApp.Register()
-				wsApp.Listen()
-
-				assertWebsocketSuccess(wsApp)
-			})
-
-			It("closes connections with backends that respond with non 101-status code", func() {
-				wsApp := test.NewHangingWebSocketApp([]route.Uri{"ws-app." + test_util.LocalhostDNS}, proxyPort, mbusClient, "")
-				wsApp.Register()
-				wsApp.Listen()
-
-				routesUri := fmt.Sprintf("http://%s:%s@%s:%d/routes", cfg.Status.User, cfg.Status.Pass, localIP, statusPort)
-
-				Eventually(func() bool { return appRegistered(routesUri, wsApp) }, "2s").Should(BeTrue())
-
-				conn, err := net.Dial("tcp", fmt.Sprintf("ws-app.%s:%d", test_util.LocalhostDNS, cfg.Port))
-				Expect(err).NotTo(HaveOccurred())
-
-				x := test_util.NewHttpConn(conn)
-
-				req := test_util.NewRequest("GET", "ws-app."+test_util.LocalhostDNS, "/chat", nil)
-				req.Header.Set("Upgrade", "websocket")
-				req.Header.Set("Connection", "upgrade")
-				x.WriteRequest(req)
-
-				responseChan := make(chan *http.Response)
-				go func() {
-					defer GinkgoRecover()
-					var resp *http.Response
-					resp, err = http.ReadResponse(x.Reader, &http.Request{})
-					Expect(err).NotTo(HaveOccurred())
-					defer resp.Body.Close()
-					responseChan <- resp
-				}()
-
-				var resp *http.Response
-				Eventually(responseChan, "9s").Should(Receive(&resp))
-				Expect(resp.StatusCode).To(Equal(404))
-
-				// client-side conn should have been closed
-				// we verify this by trying to read from it, and checking that
-				//  - the read does not block
-				//  - the read returns no data
-				//  - the read returns an error EOF
-				n, err := conn.Read(make([]byte, 100))
-				Expect(n).To(Equal(0))
-				Expect(err).To(Equal(io.EOF))
-
-				x.Close()
-			})
 		})
 	})
 
@@ -670,11 +509,11 @@ var _ = Describe("Router Integration", func() {
 			Expect(err).ToNot(HaveOccurred())
 			sendRegistration := func(port int, url string) error {
 				rm := mbus.RegistryMessage{
-					Host:                    "127.0.0.1",
-					Port:                    uint16(port),
-					Uris:                    []route.Uri{route.Uri(url)},
-					Tags:                    nil,
-					App:                     "0",
+					Host: "127.0.0.1",
+					Port: uint16(port),
+					Uris: []route.Uri{route.Uri(url)},
+					Tags: nil,
+					App:  "0",
 					StaleThresholdInSeconds: 1,
 					EndpointUpdatedAtNs:     time.Now().Add(-10 * time.Second).UnixNano(),
 					// simulate 10 seconds of latency on NATS message
---
+++ integration/tls_to_backends_test.go
@@ -0,0 +1,167 @@
+package integration
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"time"
+
+	"code.cloudfoundry.org/gorouter/config"
+	"code.cloudfoundry.org/gorouter/route"
+	"code.cloudfoundry.org/gorouter/test"
+	"code.cloudfoundry.org/gorouter/test/common"
+	"code.cloudfoundry.org/gorouter/test_util"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("TLS to backends", func() {
+	var (
+		testState           *testState
+		serverCertDomainSAN string
+		backendCertChain    test_util.CertChain
+		clientCertChain     test_util.CertChain
+		backendTLSConfig    *tls.Config
+	)
+
+	BeforeEach(func() {
+		testState = NewTestState()
+		testState.cfg.SkipSSLValidation = false
+		testState.cfg.CipherSuites = []uint16{tls.TLS_RSA_WITH_AES_256_CBC_SHA}
+
+		serverCertDomainSAN = "example.com"
+		backendCertChain = test_util.CreateSignedCertWithRootCA(test_util.CertNames{CommonName: serverCertDomainSAN})
+		testState.cfg.CACerts = string(backendCertChain.CACertPEM)
+
+		clientCertChain = test_util.CreateSignedCertWithRootCA(test_util.CertNames{CommonName: "gorouter"})
+		backendTLSConfig = backendCertChain.AsTLSConfig()
+		backendTLSConfig.ClientAuth = tls.RequireAndVerifyClientCert
+
+		// set Gorouter to use client certs
+		testState.cfg.Backends.TLSPem = config.TLSPem{
+			CertChain:  string(clientCertChain.CertPEM),
+			PrivateKey: string(clientCertChain.PrivKeyPEM),
+		}
+
+		// make backend trust the CA that signed the gorouter's client cert
+		certPool := x509.NewCertPool()
+		certPool.AddCert(clientCertChain.CACert)
+		backendTLSConfig.ClientCAs = certPool
+
+		testState.StartGorouter()
+	})
+
+	AfterEach(func() {
+		if testState != nil {
+			testState.StopAndCleanup()
+		}
+	})
+
+	FContext("websockets and TLS interaction", func() {
+		assertWebsocketSuccess := func(wsApp *common.TestApp) {
+			routesURI := fmt.Sprintf("http://%s:%s@%s:%d/routes", testState.cfg.Status.User, testState.cfg.Status.Pass, "localhost", testState.cfg.Status.Port)
+
+			Eventually(func() bool { return appRegistered(routesURI, wsApp) }, "2s").Should(BeTrue())
+
+			conn, err := net.Dial("tcp", fmt.Sprintf("ws-app.%s:%d", test_util.LocalhostDNS, testState.cfg.Port))
+			Expect(err).NotTo(HaveOccurred())
+
+			x := test_util.NewHttpConn(conn)
+
+			req := test_util.NewRequest("GET", "ws-app."+test_util.LocalhostDNS, "/chat", nil)
+			req.Header.Set("Upgrade", "websocket")
+			req.Header.Set("Connection", "upgrade")
+			x.WriteRequest(req)
+
+			resp, _ := x.ReadResponse()
+			Expect(resp.StatusCode).To(Equal(http.StatusSwitchingProtocols))
+
+			x.WriteLine("hello from client")
+			x.CheckLine("hello from server")
+
+			x.Close()
+		}
+
+		It("successfully connects with both websockets and TLS to backends", func() {
+			wsApp := test.NewWebSocketApp([]route.Uri{"ws-app." + test_util.LocalhostDNS}, testState.cfg.Port, testState.mbusClient, time.Millisecond, "")
+			wsApp.TlsRegister(serverCertDomainSAN)
+			wsApp.TlsListen(backendTLSConfig)
+
+			assertWebsocketSuccess(wsApp)
+		})
+
+		It("successfully connects with websockets but not TLS to backends", func() {
+			wsApp := test.NewWebSocketApp([]route.Uri{"ws-app." + test_util.LocalhostDNS}, testState.cfg.Port, testState.mbusClient, time.Millisecond, "")
+			wsApp.Register()
+			wsApp.Listen()
+
+			assertWebsocketSuccess(wsApp)
+		})
+
+		It("closes connections with backends that respond with non 101-status code", func() {
+			wsApp := test.NewHangingWebSocketApp([]route.Uri{"ws-app." + test_util.LocalhostDNS}, testState.cfg.Port, testState.mbusClient, "")
+			wsApp.Register()
+			wsApp.Listen()
+
+			routesURI := fmt.Sprintf("http://%s:%s@%s:%d/routes", testState.cfg.Status.User, testState.cfg.Status.Pass, localIP, testState.cfg.Status.Port)
+
+			Eventually(func() bool { return appRegistered(routesURI, wsApp) }, "2s").Should(BeTrue())
+
+			conn, err := net.Dial("tcp", fmt.Sprintf("ws-app.%s:%d", test_util.LocalhostDNS, testState.cfg.Port))
+			Expect(err).NotTo(HaveOccurred())
+
+			x := test_util.NewHttpConn(conn)
+
+			req := test_util.NewRequest("GET", "ws-app."+test_util.LocalhostDNS, "/chat", nil)
+			req.Header.Set("Upgrade", "websocket")
+			req.Header.Set("Connection", "upgrade")
+			x.WriteRequest(req)
+
+			responseChan := make(chan *http.Response)
+			go func() {
+				defer GinkgoRecover()
+				var resp *http.Response
+				resp, err = http.ReadResponse(x.Reader, &http.Request{})
+				Expect(err).NotTo(HaveOccurred())
+				defer resp.Body.Close()
+				responseChan <- resp
+			}()
+
+			var resp *http.Response
+			Eventually(responseChan, "9s").Should(Receive(&resp))
+			Expect(resp.StatusCode).To(Equal(404))
+
+			// client-side conn should have been closed
+			// we verify this by trying to read from it, and checking that
+			//  - the read does not block
+			//  - the read returns no data
+			//  - the read returns an error EOF
+			n, err := conn.Read(make([]byte, 100))
+			Expect(n).To(Equal(0))
+			Expect(err).To(Equal(io.EOF))
+
+			x.Close()
+		})
+	})
+
+	It("successfully establishes a mutual TLS connection with backend", func() {
+		runningApp1 := test.NewGreetApp([]route.Uri{"some-app-expecting-client-certs." + test_util.LocalhostDNS}, testState.cfg.Port, testState.mbusClient, nil)
+		runningApp1.TlsRegister(serverCertDomainSAN)
+		runningApp1.TlsListen(backendTLSConfig)
+		heartbeatInterval := 200 * time.Millisecond
+		runningTicker := time.NewTicker(heartbeatInterval)
+		go func() {
+			for {
+				<-runningTicker.C
+				runningApp1.TlsRegister(serverCertDomainSAN)
+			}
+		}()
+		routesURI := fmt.Sprintf("http://%s:%s@%s:%d/routes", testState.cfg.Status.User, testState.cfg.Status.Pass, "localhost", testState.cfg.Status.Port)
+
+		Eventually(func() bool { return appRegistered(routesURI, runningApp1) }, "2s").Should(BeTrue())
+		runningApp1.VerifyAppStatus(200)
+	})
+})
