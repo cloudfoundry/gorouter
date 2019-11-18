caf68ebed61e4d91ed3da55bbe40ffc901d61da6
toby lorne <toby@toby.codes>
2019-11-18 23:47:28 +0000

    Remove unused code from zipkin handler
    Signed-off-by: toby lorne <toby@toby.codes>

--- handlers/zipkin.go
+++ handlers/zipkin.go
@@ -133,12 +133,3 @@ func (z *Zipkin) HeadersToLog() []string {
 		B3Header,
 	}
 }
-
-func contains(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
-}
