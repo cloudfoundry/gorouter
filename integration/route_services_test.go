425e78cfa5f1c337b4e573c99102c552d07ea60d
Bruce Ricard <bricard@pivotal.io>
2020-02-28 10:58:25 -0800

    fix: reformatting
    Co-authored-by: Aidan Obley <aobley@pivotal.io>
    Co-authored-by: Clay Kauzlaric <ckauzlaric@pivotal.io>

--- integration/route_services_test.go
+++ integration/route_services_test.go
@@ -26,76 +26,77 @@ var _ = Describe("Route services", func() {
 		}
 	})
 
-	Context("Headers", func() {
+	Context("Happy Path", func() {
 		const (
-			TEST_STATUS_CODE = 477
-			appHostname      = "app-with-route-service.some.domain"
+			appHostname = "app-with-route-service.some.domain"
 		)
 
-		var (
-			testApp      *httptest.Server
-			routeService *httptest.Server
-		)
-		BeforeEach(func() {
-			testApp = httptest.NewServer(http.HandlerFunc(
-				func(w http.ResponseWriter, r *http.Request) {
-					w.WriteHeader(200)
-					_, _ = w.Write([]byte("I'm the app"))
-				}))
-
-			routeService = httptest.NewServer(
-				http.HandlerFunc(
+		Context("When an app is registered with a simple route service", func() {
+			var (
+				testApp      *httptest.Server
+				routeService *httptest.Server
+			)
+			BeforeEach(func() {
+				testApp = httptest.NewServer(http.HandlerFunc(
 					func(w http.ResponseWriter, r *http.Request) {
-						defer GinkgoRecover()
-
-						forwardedURL := r.Header.Get("X-CF-Forwarded-Url")
-						sigHeader := r.Header.Get("X-Cf-Proxy-Signature")
-						metadata := r.Header.Get("X-Cf-Proxy-Metadata")
-
-						req := testState.newRequest(forwardedURL)
-
-						req.Header.Add("X-CF-Forwarded-Url", forwardedURL)
-						req.Header.Add("X-Cf-Proxy-Metadata", metadata)
-						req.Header.Add("X-Cf-Proxy-Signature", sigHeader)
-
-						res, err := testState.routeServiceClient.Do(req)
-						defer res.Body.Close()
-						Expect(err).ToNot(HaveOccurred())
-						Expect(res.StatusCode).To(Equal(http.StatusOK))
-
-						body, err := ioutil.ReadAll(res.Body)
-						Expect(err).ToNot(HaveOccurred())
-						Expect(body).To(Equal([]byte("I'm the app")))
-
-						w.WriteHeader(res.StatusCode)
-						_, _ = w.Write([]byte("I'm the route service"))
+						w.WriteHeader(200)
+						_, _ = w.Write([]byte("I'm the app"))
 					}))
 
-			testState.registerWithInternalRouteService(
-				testApp,
-				routeService,
-				appHostname,
-				testState.cfg.SSLPort,
-			)
+				routeService = httptest.NewServer(
+					http.HandlerFunc(
+						func(w http.ResponseWriter, r *http.Request) {
+							defer GinkgoRecover()
 
-		})
+							forwardedURL := r.Header.Get("X-CF-Forwarded-Url")
+							sigHeader := r.Header.Get("X-Cf-Proxy-Signature")
+							metadata := r.Header.Get("X-Cf-Proxy-Metadata")
 
-		AfterEach(func() {
-			routeService.Close()
-			testApp.Close()
-		})
+							req := testState.newRequest(forwardedURL)
 
-		It("URL-encrypts and decrypts properly", func() {
-			req := testState.newRequest(
-				fmt.Sprintf("https://%s?%s", appHostname, "p=a%0Ab"),
-			)
+							req.Header.Add("X-CF-Forwarded-Url", forwardedURL)
+							req.Header.Add("X-Cf-Proxy-Metadata", metadata)
+							req.Header.Add("X-Cf-Proxy-Signature", sigHeader)
 
-			res, err := testState.client.Do(req)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(res.StatusCode).To(Equal(http.StatusOK))
-			body, err := ioutil.ReadAll(res.Body)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(body).To(Equal([]byte("I'm the route service")))
+							res, err := testState.routeServiceClient.Do(req)
+							defer res.Body.Close()
+							Expect(err).ToNot(HaveOccurred())
+							Expect(res.StatusCode).To(Equal(http.StatusOK))
+
+							body, err := ioutil.ReadAll(res.Body)
+							Expect(err).ToNot(HaveOccurred())
+							Expect(body).To(Equal([]byte("I'm the app")))
+
+							w.WriteHeader(res.StatusCode)
+							_, _ = w.Write([]byte("I'm the route service"))
+						}))
+
+				testState.registerWithInternalRouteService(
+					testApp,
+					routeService,
+					appHostname,
+					testState.cfg.SSLPort,
+				)
+
+			})
+
+			AfterEach(func() {
+				routeService.Close()
+				testApp.Close()
+			})
+
+			It("succeeds", func() {
+				req := testState.newRequest(
+					fmt.Sprintf("https://%s", appHostname),
+				)
+
+				res, err := testState.client.Do(req)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(res.StatusCode).To(Equal(http.StatusOK))
+				body, err := ioutil.ReadAll(res.Body)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(body).To(Equal([]byte("I'm the route service")))
+			})
 		})
 	})
 })
