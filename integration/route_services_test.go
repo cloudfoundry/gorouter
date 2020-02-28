1367f6df21cac34ee6ca7378cae8e6bf8566364e
Aidan Obley <aobley@pivotal.io>
2020-02-28 10:54:34 -0800

    refactor: backfill route service test
    * Add end to end route service test
    
    Co-authored-by: Clay Kauzlaric <ckauzlaric@pivotal.io>
    Co-authored-by: Bruce Ricard <bricard@pivotal.io>

---
+++ integration/route_services_test.go
@@ -0,0 +1,101 @@
+package integration
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Route services", func() {
+
+	var testState *testState
+
+	BeforeEach(func() {
+		testState = NewTestState()
+
+		testState.StartGorouter()
+	})
+
+	AfterEach(func() {
+		if testState != nil {
+			testState.StopAndCleanup()
+		}
+	})
+
+	Context("Headers", func() {
+		const (
+			TEST_STATUS_CODE = 477
+			appHostname      = "app-with-route-service.some.domain"
+		)
+
+		var (
+			testApp      *httptest.Server
+			routeService *httptest.Server
+		)
+		BeforeEach(func() {
+			testApp = httptest.NewServer(http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(200)
+					_, _ = w.Write([]byte("I'm the app"))
+				}))
+
+			routeService = httptest.NewServer(
+				http.HandlerFunc(
+					func(w http.ResponseWriter, r *http.Request) {
+						defer GinkgoRecover()
+
+						forwardedURL := r.Header.Get("X-CF-Forwarded-Url")
+						sigHeader := r.Header.Get("X-Cf-Proxy-Signature")
+						metadata := r.Header.Get("X-Cf-Proxy-Metadata")
+
+						req := testState.newRequest(forwardedURL)
+
+						req.Header.Add("X-CF-Forwarded-Url", forwardedURL)
+						req.Header.Add("X-Cf-Proxy-Metadata", metadata)
+						req.Header.Add("X-Cf-Proxy-Signature", sigHeader)
+
+						res, err := testState.routeServiceClient.Do(req)
+						defer res.Body.Close()
+						Expect(err).ToNot(HaveOccurred())
+						Expect(res.StatusCode).To(Equal(http.StatusOK))
+
+						body, err := ioutil.ReadAll(res.Body)
+						Expect(err).ToNot(HaveOccurred())
+						Expect(body).To(Equal([]byte("I'm the app")))
+
+						w.WriteHeader(res.StatusCode)
+						_, _ = w.Write([]byte("I'm the route service"))
+					}))
+
+			testState.registerWithInternalRouteService(
+				testApp,
+				routeService,
+				appHostname,
+				testState.cfg.SSLPort,
+			)
+
+		})
+
+		AfterEach(func() {
+			routeService.Close()
+			testApp.Close()
+		})
+
+		FIt("URL-encrypts and decrypts properly", func() {
+			req := testState.newRequest(
+				fmt.Sprintf("https://%s?%s", appHostname, "p=a%0Ab"),
+			)
+
+			res, err := testState.client.Do(req)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(res.StatusCode).To(Equal(http.StatusOK))
+			body, err := ioutil.ReadAll(res.Body)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(body).To(Equal([]byte("I'm the route service")))
+		})
+	})
+})
