2f9cd81b494441d61b80a5effa0590dd9afc883a
David McClure <dmcclure@vmware.com>
2020-08-20 17:18:24 +0000

    Add guidance for adding retriable classifiers
    RetriableClassifiers include backend errors that are safe to retry
    
    Backend errors are only safe to retry if we can be certain that they have
    occurred before any http request data has been sent from gorouter to the
    backend application.
    
    Otherwise, there’s risk of a mutating non-idempotent request (e.g. send
    payment) being silently retried without the client knowing.

--- proxy/fails/classifier_group.go
+++ proxy/fails/classifier_group.go
@@ -2,6 +2,14 @@ package fails
 
 type ClassifierGroup []Classifier
 
+// RetriableClassifiers include backend errors that are safe to retry
+//
+// Backend errors are only safe to retry if we can be certain that they have
+// occurred before any http request data has been sent from gorouter to the
+// backend application.
+//
+// Otherwise, there’s risk of a mutating non-idempotent request (e.g. send
+// payment) being silently retried without the client knowing.
 var RetriableClassifiers = ClassifierGroup{
 	Dial,
 	AttemptedTLSWithNonTLSBackend,
