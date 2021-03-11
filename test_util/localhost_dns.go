09a22fa4294729a16a1656efc3fd5a09356dc7c2
Renee Chu <reneec@vmware.com>
2021-03-11 22:03:56 +0000

    add comment about how testing dns works
    * our dns entry got deleted
    * which caused many integration + units to fail
    * and took awhile to debug
    * this comment should help future devs
    
    [#177295551](https://www.pivotaltracker.com/story/show/177295551)

--- test_util/localhost_dns.go
+++ test_util/localhost_dns.go
@@ -1,3 +1,5 @@
 package test_util
 
+// Our tests assume that there exists a DNS entry *.localhost.routing.cf-app.com that maps to 127.0.0.1
+// If that DNS entry does not work, then many, many tests will fail
 const LocalhostDNS = "localhost.routing.cf-app.com"
