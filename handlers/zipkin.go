e5d7aab0b68ed16f3ea36caa2a739e68dc5c2779
Maria Shaldybin <mariash@vmware.com>
2021-11-05 17:44:42 +0000

    Change Zipking TraceID to 16 byte
    to comply with the w3c standard https://www.w3.org/TR/trace-context/#trace-id
    Use external library github.com/openzipkin/zipkin-go for TraceID
    generation and writing single b3 header.
    
    [#179870729](https://www.pivotaltracker.com/story/show/179870729)

--- handlers/zipkin.go
+++ handlers/zipkin.go
@@ -1,25 +1,14 @@
 package handlers
 
 import (
-	"encoding/hex"
 	"net/http"
-	"strconv"
 
+	"github.com/openzipkin/zipkin-go/idgenerator"
+	"github.com/openzipkin/zipkin-go/propagation/b3"
 	"github.com/uber-go/zap"
 	"github.com/urfave/negroni"
 
 	"code.cloudfoundry.org/gorouter/logger"
-
-	"code.cloudfoundry.org/gorouter/common/secure"
-)
-
-const (
-	B3Header             = "b3"
-	B3TraceIdHeader      = "X-B3-TraceId"
-	B3SpanIdHeader       = "X-B3-SpanId"
-	B3ParentSpanIdHeader = "X-B3-ParentSpanId"
-	B3SampledHeader      = "X-B3-Sampled"
-	B3FlagsHeader        = "X-B3-Flags"
 )
 
 // Zipkin is a handler that sets Zipkin headers on requests
@@ -44,79 +33,42 @@ func (z *Zipkin) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.Ha
 		return
 	}
 
-	existingContext := r.Header.Get(B3Header)
+	existingContext := r.Header.Get(b3.Context)
 	if existingContext != "" {
 		z.logger.Debug("b3-header-exists",
-			zap.String("B3Header", existingContext),
+			zap.String("b3", existingContext),
 		)
 
 		return
 	}
 
-	existingTraceID := r.Header.Get(B3TraceIdHeader)
-	existingSpanID := r.Header.Get(B3SpanIdHeader)
+	existingTraceID := r.Header.Get(b3.TraceID)
+	existingSpanID := r.Header.Get(b3.SpanID)
 	if existingTraceID == "" || existingSpanID == "" {
-		traceID, err := generateSpanID()
-		if err != nil {
-			z.logger.Info("failed-to-create-b3-trace-id", zap.Error(err))
-			return
-		}
+		traceID := idgenerator.NewRandom128().TraceID().String()
 
-		r.Header.Set(B3TraceIdHeader, traceID)
-		r.Header.Set(B3SpanIdHeader, traceID)
-		r.Header.Set(B3Header, traceID+"-"+traceID)
+		r.Header.Set(b3.TraceID, traceID)
+		r.Header.Set(b3.SpanID, traceID)
+		r.Header.Set(b3.Context, traceID+"-"+traceID)
 	} else {
-		r.Header.Set(B3Header, BuildB3SingleHeader(
+		sc, err := b3.ParseHeaders(
 			existingTraceID,
 			existingSpanID,
-			r.Header.Get(B3SampledHeader),
-			r.Header.Get(B3FlagsHeader),
-			r.Header.Get(B3ParentSpanIdHeader),
-		))
+			r.Header.Get(b3.ParentSpanID),
+			r.Header.Get(b3.Sampled),
+			r.Header.Get(b3.Flags),
+		)
+		if err != nil {
+			z.logger.Info("failed-to-parse-b3-trace-id", zap.Error(err))
+			return
+		}
+		r.Header.Set(b3.Context, b3.BuildSingleHeader(*sc))
 
 		z.logger.Debug("b3-trace-id-span-id-header-exists",
-			zap.String("B3TraceIdHeader", existingTraceID),
-			zap.String("B3SpanIdHeader", existingSpanID),
+			zap.String("traceID", existingTraceID),
+			zap.String("spanID", existingSpanID),
 		)
 	}
-}
-
-func generateSpanID() (string, error) {
-	randBytes, err := secure.RandomBytes(8)
-	if err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(randBytes), nil
-}
-
-// BuildB3SingleHeader assembles the B3 single header based on existing trace
-// values
-func BuildB3SingleHeader(traceID, spanID, sampling, flags, parentSpanID string) string {
-	if traceID == "" || spanID == "" {
-		return ""
-	}
-
-	if sampling == "" && flags == "" {
-		return traceID + "-" + spanID
-	}
-
-	samplingBit := "0"
-	if flags == "1" {
-		samplingBit = "d"
-	} else if s, err := strconv.ParseBool(sampling); err == nil {
-		if s {
-			samplingBit = "1"
-		}
-	} else {
-		return traceID + "-" + spanID
-	}
-
-	if parentSpanID == "" {
-		return traceID + "-" + spanID + "-" + samplingBit
-	}
-
-	return traceID + "-" + spanID + "-" + samplingBit + "-" + parentSpanID
 }
 
 // HeadersToLog specifies the headers which should be logged if Zipkin headers
@@ -127,9 +79,9 @@ func (z *Zipkin) HeadersToLog() []string {
 	}
 
 	return []string{
-		B3TraceIdHeader,
-		B3SpanIdHeader,
-		B3ParentSpanIdHeader,
-		B3Header,
+		b3.TraceID,
+		b3.SpanID,
+		b3.ParentSpanID,
+		b3.Context,
 	}
 }
--- handlers/zipkin_test.go
+++ handlers/zipkin_test.go
@@ -10,12 +10,13 @@ import (
 	"code.cloudfoundry.org/gorouter/logger"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/openzipkin/zipkin-go/propagation/b3"
 )
 
 // 64-bit random hexadecimal string
 const (
-	b3IDRegex      = `^[[:xdigit:]]{16,32}$`
-	b3Regex        = `^[[:xdigit:]]{16,32}-[[:xdigit:]]{16}(-[01d](-[[:xdigit:]]{16,32})?)?$`
+	b3IDRegex      = `^[[:xdigit:]]{32}$`
+	b3Regex        = `^[[:xdigit:]]{32}-[[:xdigit:]]{32}(-[01d](-[[:xdigit:]]{32})?)?$`
 	b3TraceID      = "7f46165474d11ee5836777d85df2cdab"
 	b3SpanID       = "54ebcb82b14862d9"
 	b3ParentSpanID = "e56b75d6af463476"
@@ -52,70 +53,97 @@ var _ = Describe("Zipkin", func() {
 
 		It("sets zipkin headers", func() {
 			handler.ServeHTTP(resp, req, nextHandler)
-			Expect(req.Header.Get(handlers.B3SpanIdHeader)).ToNot(BeEmpty())
-			Expect(req.Header.Get(handlers.B3TraceIdHeader)).ToNot(BeEmpty())
-			Expect(req.Header.Get(handlers.B3ParentSpanIdHeader)).To(BeEmpty())
-			Expect(req.Header.Get(handlers.B3Header)).ToNot(BeEmpty())
+			Expect(req.Header.Get(b3.SpanID)).ToNot(BeEmpty())
+			Expect(req.Header.Get(b3.TraceID)).ToNot(BeEmpty())
+			Expect(req.Header.Get(b3.ParentSpanID)).To(BeEmpty())
+			Expect(req.Header.Get(b3.Context)).ToNot(BeEmpty())
 
 			Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 		})
 
 		Context("with B3TraceIdHeader, B3SpanIdHeader and B3ParentSpanIdHeader already set", func() {
 			BeforeEach(func() {
-				req.Header.Set(handlers.B3TraceIdHeader, b3TraceID)
-				req.Header.Set(handlers.B3SpanIdHeader, b3SpanID)
-				req.Header.Set(handlers.B3ParentSpanIdHeader, b3ParentSpanID)
+				req.Header.Set(b3.TraceID, b3TraceID)
+				req.Header.Set(b3.SpanID, b3SpanID)
+				req.Header.Set(b3.ParentSpanID, b3ParentSpanID)
 			})
 
 			It("doesn't overwrite the B3ParentSpanIdHeader", func() {
 				handler.ServeHTTP(resp, req, nextHandler)
-				Expect(req.Header.Get(handlers.B3ParentSpanIdHeader)).To(Equal(b3ParentSpanID))
+				Expect(req.Header.Get(b3.ParentSpanID)).To(Equal(b3ParentSpanID))
 
 				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 			})
 
 			It("doesn't overwrite the B3SpanIdHeader", func() {
 				handler.ServeHTTP(resp, req, nextHandler)
-				Expect(req.Header.Get(handlers.B3SpanIdHeader)).To(Equal(b3SpanID))
+				Expect(req.Header.Get(b3.SpanID)).To(Equal(b3SpanID))
 
 				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 			})
 
 			It("doesn't overwrite the B3TraceIdHeader", func() {
 				handler.ServeHTTP(resp, req, nextHandler)
-				Expect(req.Header.Get(handlers.B3TraceIdHeader)).To(Equal(b3TraceID))
+				Expect(req.Header.Get(b3.TraceID)).To(Equal(b3TraceID))
 
 				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 			})
 		})
+
 		Context("with B3TraceIdHeader and B3SpanIdHeader already set", func() {
 			BeforeEach(func() {
-				req.Header.Set(handlers.B3TraceIdHeader, b3TraceID)
-				req.Header.Set(handlers.B3SpanIdHeader, b3SpanID)
+				req.Header.Set(b3.TraceID, b3TraceID)
+				req.Header.Set(b3.SpanID, b3SpanID)
 			})
 
 			It("propagates the B3Header", func() {
-				req.Header.Set(handlers.B3SampledHeader, "true")
-				req.Header.Set(handlers.B3ParentSpanIdHeader, b3ParentSpanID)
+				handler.ServeHTTP(resp, req, nextHandler)
+
+				Expect(req.Header.Get(b3.Context)).To(Equal(b3TraceID + "-" + b3SpanID))
+				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
+			})
+
+			It("propagates the B3Header with Sampled header", func() {
+				req.Header.Set(b3.Sampled, "true")
 
 				handler.ServeHTTP(resp, req, nextHandler)
 
-				Expect(req.Header.Get(handlers.B3Header)).To(Equal(b3TraceID + "-" + b3SpanID + "-1-" + b3ParentSpanID))
+				Expect(req.Header.Get(b3.Context)).To(Equal(b3TraceID + "-" + b3SpanID + "-1"))
+				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
+			})
+
+			It("propagates the B3Header with Flags header", func() {
+				req.Header.Set(b3.Flags, "1")
+				req.Header.Set(b3.Sampled, "false")
+
+				handler.ServeHTTP(resp, req, nextHandler)
+
+				Expect(req.Header.Get(b3.Context)).To(Equal(b3TraceID + "-" + b3SpanID + "-d"))
+				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
+			})
+
+			It("propagates the B3Header with ParentSpanID header", func() {
+				req.Header.Set(b3.Sampled, "false")
+				req.Header.Set(b3.ParentSpanID, b3ParentSpanID)
+
+				handler.ServeHTTP(resp, req, nextHandler)
+
+				Expect(req.Header.Get(b3.Context)).To(Equal(b3TraceID + "-" + b3SpanID + "-0-" + b3ParentSpanID))
 				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 			})
 
 			It("doesn't overwrite the B3SpanIdHeader", func() {
 				handler.ServeHTTP(resp, req, nextHandler)
-				Expect(req.Header.Get(handlers.B3SpanIdHeader)).To(Equal(b3SpanID))
-				Expect(req.Header.Get(handlers.B3Header)).To(Equal(b3TraceID + "-" + b3SpanID))
-				Expect(req.Header.Get(handlers.B3ParentSpanIdHeader)).To(BeEmpty())
+				Expect(req.Header.Get(b3.SpanID)).To(Equal(b3SpanID))
+				Expect(req.Header.Get(b3.Context)).To(Equal(b3TraceID + "-" + b3SpanID))
+				Expect(req.Header.Get(b3.ParentSpanID)).To(BeEmpty())
 
 				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 			})
 
 			It("doesn't overwrite the B3TraceIdHeader", func() {
 				handler.ServeHTTP(resp, req, nextHandler)
-				Expect(req.Header.Get(handlers.B3TraceIdHeader)).To(Equal(b3TraceID))
+				Expect(req.Header.Get(b3.TraceID)).To(Equal(b3TraceID))
 
 				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 			})
@@ -123,15 +151,15 @@ var _ = Describe("Zipkin", func() {
 
 		Context("with only B3SpanIdHeader set", func() {
 			BeforeEach(func() {
-				req.Header.Set(handlers.B3SpanIdHeader, b3SpanID)
+				req.Header.Set(b3.SpanID, b3SpanID)
 			})
 
 			It("adds the B3TraceIdHeader and overwrites the SpanId", func() {
 				handler.ServeHTTP(resp, req, nextHandler)
-				Expect(req.Header.Get(handlers.B3TraceIdHeader)).To(MatchRegexp(b3IDRegex))
-				Expect(req.Header.Get(handlers.B3SpanIdHeader)).To(MatchRegexp(b3IDRegex))
-				Expect(req.Header.Get(handlers.B3ParentSpanIdHeader)).To(BeEmpty())
-				Expect(req.Header.Get(handlers.B3Header)).To(MatchRegexp(b3Regex))
+				Expect(req.Header.Get(b3.TraceID)).To(MatchRegexp(b3IDRegex))
+				Expect(req.Header.Get(b3.SpanID)).To(MatchRegexp(b3IDRegex))
+				Expect(req.Header.Get(b3.ParentSpanID)).To(BeEmpty())
+				Expect(req.Header.Get(b3.Context)).To(MatchRegexp(b3Regex))
 
 				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 			})
@@ -139,15 +167,15 @@ var _ = Describe("Zipkin", func() {
 
 		Context("with only B3TraceIdHeader set", func() {
 			BeforeEach(func() {
-				req.Header.Set(handlers.B3TraceIdHeader, b3TraceID)
+				req.Header.Set(b3.TraceID, b3TraceID)
 			})
 
 			It("overwrites the B3TraceIdHeader and adds a SpanId", func() {
 				handler.ServeHTTP(resp, req, nextHandler)
-				Expect(req.Header.Get(handlers.B3TraceIdHeader)).To(MatchRegexp(b3IDRegex))
-				Expect(req.Header.Get(handlers.B3SpanIdHeader)).To(MatchRegexp(b3IDRegex))
-				Expect(req.Header.Get(handlers.B3ParentSpanIdHeader)).To(BeEmpty())
-				Expect(req.Header.Get(handlers.B3Header)).To(MatchRegexp(b3Regex))
+				Expect(req.Header.Get(b3.TraceID)).To(MatchRegexp(b3IDRegex))
+				Expect(req.Header.Get(b3.SpanID)).To(MatchRegexp(b3IDRegex))
+				Expect(req.Header.Get(b3.ParentSpanID)).To(BeEmpty())
+				Expect(req.Header.Get(b3.Context)).To(MatchRegexp(b3Regex))
 
 				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 			})
@@ -155,15 +183,15 @@ var _ = Describe("Zipkin", func() {
 
 		Context("with B3Header already set", func() {
 			BeforeEach(func() {
-				req.Header.Set(handlers.B3Header, b3Single)
+				req.Header.Set(b3.Context, b3Single)
 			})
 
 			It("doesn't overwrite the B3Header", func() {
 				handler.ServeHTTP(resp, req, nextHandler)
-				Expect(req.Header.Get(handlers.B3Header)).To(Equal(b3Single))
-				Expect(req.Header.Get(handlers.B3TraceIdHeader)).To(BeEmpty())
-				Expect(req.Header.Get(handlers.B3SpanIdHeader)).To(BeEmpty())
-				Expect(req.Header.Get(handlers.B3ParentSpanIdHeader)).To(BeEmpty())
+				Expect(req.Header.Get(b3.Context)).To(Equal(b3Single))
+				Expect(req.Header.Get(b3.TraceID)).To(BeEmpty())
+				Expect(req.Header.Get(b3.SpanID)).To(BeEmpty())
+				Expect(req.Header.Get(b3.ParentSpanID)).To(BeEmpty())
 
 				Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 			})
@@ -177,35 +205,12 @@ var _ = Describe("Zipkin", func() {
 
 		It("doesn't set any headers", func() {
 			handler.ServeHTTP(resp, req, nextHandler)
-			Expect(req.Header.Get(handlers.B3SpanIdHeader)).To(BeEmpty())
-			Expect(req.Header.Get(handlers.B3TraceIdHeader)).To(BeEmpty())
-			Expect(req.Header.Get(handlers.B3ParentSpanIdHeader)).To(BeEmpty())
-			Expect(req.Header.Get(handlers.B3Header)).To(BeEmpty())
+			Expect(req.Header.Get(b3.SpanID)).To(BeEmpty())
+			Expect(req.Header.Get(b3.TraceID)).To(BeEmpty())
+			Expect(req.Header.Get(b3.ParentSpanID)).To(BeEmpty())
+			Expect(req.Header.Get(b3.Context)).To(BeEmpty())
 
 			Expect(nextCalled).To(BeTrue(), "Expected the next handler to be called.")
 		})
-	})
-})
-
-var _ = Describe("B3SingleHeaderBuilder", func() {
-	It("builds B3Header correctly", func() {
-		cases := []struct {
-			traceID        string
-			spanID         string
-			sampling       string
-			flags          string
-			parentID       string
-			expectedHeader string
-		}{
-			{traceID: "", spanID: "", expectedHeader: ""},
-			{traceID: b3TraceID, spanID: b3SpanID, expectedHeader: b3TraceID + "-" + b3SpanID},
-			{traceID: b3TraceID, spanID: b3SpanID, sampling: "true", expectedHeader: b3TraceID + "-" + b3SpanID + "-1"},
-			{traceID: b3TraceID, spanID: b3SpanID, flags: "1", sampling: "false", expectedHeader: b3TraceID + "-" + b3SpanID + "-d"},
-			{traceID: b3TraceID, spanID: b3SpanID, sampling: "false", parentID: b3ParentSpanID, expectedHeader: b3TraceID + "-" + b3SpanID + "-0-" + b3ParentSpanID},
-		}
-
-		for _, c := range cases {
-			Expect(handlers.BuildB3SingleHeader(c.traceID, c.spanID, c.sampling, c.flags, c.parentID)).To(Equal(c.expectedHeader))
-		}
 	})
 })
