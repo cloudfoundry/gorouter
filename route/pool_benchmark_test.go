1a73c65bbb148d499192122831cd8d0b6c351edd
Alexander Lais <alexander.lais@sap.com>
2023-12-04 14:06:27 +0100

    test: fix govet for pool.Endpoint.Equals() tests

--- route/pool_benchmark_test.go
+++ route/pool_benchmark_test.go
@@ -32,8 +32,19 @@ var (
 		UpdatedAt:            time.Time{},
 		RoundTripperInit:     sync.Once{},
 	}
-	endpoint3 = endpoint1
-	result    = false
+	endpoint3 = route.Endpoint{
+		ApplicationId:        "abc",
+		Protocol:             "http2",
+		Tags:                 map[string]string{"tag1": "value1", "tag2": "value2"},
+		ServerCertDomainSAN:  "host.domain.tld",
+		PrivateInstanceId:    "1234-5678-91011-0000",
+		PrivateInstanceIndex: "",
+		Stats:                nil,
+		IsolationSegment:     "",
+		UpdatedAt:            time.Time{},
+		RoundTripperInit:     sync.Once{},
+	}
+	result = false
 )
 
 func BenchmarkEndpointEquals(b *testing.B) {
@@ -49,4 +60,3 @@ func BenchmarkEndpointNotEquals(b *testing.B) {
 	}
 	b.ReportAllocs()
 }
-
