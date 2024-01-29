3438d762ff1719f253c2690d8baeb87a1af2a308
Geoff Franks <geoff.franks@broadcom.com>
2024-01-29 15:12:55 +0000

    Convert our processCipherSuites functionality to autogenerate
    Use golang's supported CipherSuites() and InsecureCipherSuites()
    to populate our mapping of cipher suite names to ID. However we have
    supported legacy cipher suite names, as well as openssl cipher suite
    names in the past, so those remain explicitly added.

--- config/config.go
+++ config/config.go
@@ -793,54 +793,36 @@ func (c *Config) Process() error {
 }
 
 func (c *Config) processCipherSuites() ([]uint16, error) {
+	// legacy/openssl formatted values that we've supported in the past
 	cipherMap := map[string]uint16{
-		"RC4-SHA":                                 0x0005, // openssl formatted values
-		"DES-CBC3-SHA":                            0x000a,
-		"AES128-SHA":                              0x002f,
-		"AES256-SHA":                              0x0035,
-		"AES128-SHA256":                           0x003c,
-		"AES128-GCM-SHA256":                       0x009c,
-		"AES256-GCM-SHA384":                       0x009d,
-		"ECDHE-ECDSA-RC4-SHA":                     0xc007,
-		"ECDHE-ECDSA-AES128-SHA":                  0xc009,
-		"ECDHE-ECDSA-AES256-SHA":                  0xc00a,
-		"ECDHE-RSA-RC4-SHA":                       0xc011,
-		"ECDHE-RSA-DES-CBC3-SHA":                  0xc012,
-		"ECDHE-RSA-AES128-SHA":                    0xc013,
-		"ECDHE-RSA-AES256-SHA":                    0xc014,
-		"ECDHE-ECDSA-AES128-SHA256":               0xc023,
-		"ECDHE-RSA-AES128-SHA256":                 0xc027,
-		"ECDHE-RSA-AES128-GCM-SHA256":             0xc02f,
-		"ECDHE-ECDSA-AES128-GCM-SHA256":           0xc02b,
-		"ECDHE-RSA-AES256-GCM-SHA384":             0xc030,
-		"ECDHE-ECDSA-AES256-GCM-SHA384":           0xc02c,
-		"ECDHE-RSA-CHACHA20-POLY1305":             0xcca8,
-		"ECDHE-ECDSA-CHACHA20-POLY1305":           0xcca9,
-		"TLS_RSA_WITH_RC4_128_SHA":                0x0005, // RFC formatted values
-		"TLS_RSA_WITH_3DES_EDE_CBC_SHA":           0x000a,
-		"TLS_RSA_WITH_AES_128_CBC_SHA":            0x002f,
-		"TLS_RSA_WITH_AES_256_CBC_SHA":            0x0035,
-		"TLS_RSA_WITH_AES_128_CBC_SHA256":         0x003c,
-		"TLS_RSA_WITH_AES_128_GCM_SHA256":         0x009c,
-		"TLS_RSA_WITH_AES_256_GCM_SHA384":         0x009d,
-		"TLS_ECDHE_ECDSA_WITH_RC4_128_SHA":        0xc007,
-		"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA":    0xc009,
-		"TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA":    0xc00a,
-		"TLS_ECDHE_RSA_WITH_RC4_128_SHA":          0xc011,
-		"TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA":     0xc012,
-		"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA":      0xc013,
-		"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA":      0xc014,
-		"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256": 0xc023,
-		"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256":   0xc027,
-		"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256":   0xc02f,
-		"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256": 0xc02b,
-		"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384":   0xc030,
-		"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384": 0xc02c,
-		"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305":    0xcca8,
-		"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305":  0xcca9,
-		"TLS_AES_128_GCM_SHA256":                  0x1301,
-		"TLS_AES_256_GCM_SHA384":                  0x1302,
-		"TLS_CHACHA20_POLY1305_SHA256":            0x1303,
+		"RC4-SHA":                                0x0005, // openssl formatted values
+		"DES-CBC3-SHA":                           0x000a,
+		"AES128-SHA":                             0x002f,
+		"AES256-SHA":                             0x0035,
+		"AES128-SHA256":                          0x003c,
+		"AES128-GCM-SHA256":                      0x009c,
+		"AES256-GCM-SHA384":                      0x009d,
+		"ECDHE-ECDSA-RC4-SHA":                    0xc007,
+		"ECDHE-ECDSA-AES128-SHA":                 0xc009,
+		"ECDHE-ECDSA-AES256-SHA":                 0xc00a,
+		"ECDHE-RSA-RC4-SHA":                      0xc011,
+		"ECDHE-RSA-DES-CBC3-SHA":                 0xc012,
+		"ECDHE-RSA-AES128-SHA":                   0xc013,
+		"ECDHE-RSA-AES256-SHA":                   0xc014,
+		"ECDHE-ECDSA-AES128-SHA256":              0xc023,
+		"ECDHE-RSA-AES128-SHA256":                0xc027,
+		"ECDHE-RSA-AES128-GCM-SHA256":            0xc02f,
+		"ECDHE-ECDSA-AES128-GCM-SHA256":          0xc02b,
+		"ECDHE-RSA-AES256-GCM-SHA384":            0xc030,
+		"ECDHE-ECDSA-AES256-GCM-SHA384":          0xc02c,
+		"ECDHE-RSA-CHACHA20-POLY1305":            0xcca8,
+		"ECDHE-ECDSA-CHACHA20-POLY1305":          0xcca9,
+		"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305":   0xcca8,
+		"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305": 0xcca9,
+	}
+	// generate the remaining suites based off of what golang has implemented using RFC names
+	for _, suite := range append(tls.CipherSuites(), tls.InsecureCipherSuites()...) {
+		cipherMap[suite.Name] = suite.ID
 	}
 
 	var ciphers []string
